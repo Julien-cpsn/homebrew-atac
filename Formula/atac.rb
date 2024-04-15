cb86e660a81aee8bba5677fff0a857dc13dc7ac7
Julien C <64107022+Julien-cpsn@users.noreply.github.com>
2024-04-15 20:15:29 +0200

    Update atac.rb

--- Formula/atac.rb
+++ Formula/atac.rb
@@ -1,9 +1,9 @@
 class Atac < Formula
   desc "A simple API client (postman like) in your terminal"
   homepage "https://github.com/Julien-cpsn/ATAC"
-  url "https://github.com/Julien-cpsn/ATAC/releases/download/v0.11.2/atac-universal-apple-darwin.tar.gz"
+  url "https://github.com/Julien-cpsn/ATAC/releases/download/v0.11.2/atac-x86_64-unknown-linux-gnu.tar.gz"
   version "0.11.2"
-  sha256 "7838ac55e0681b9bb25d21b1775a7f0612993212ba9eadbcd3def12345b2e1b1"
+  sha256 "19a8ee3580d028f5c92338be22f10f0b9241ece19530f2c514bfdcc51c668470"
   license "MIT"
 
   def install
