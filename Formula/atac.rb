8b0273605d8b40c7fe98e39301a0c0ae2339e09b
Julien C <64107022+Julien-cpsn@users.noreply.github.com>
2024-04-15 23:02:19 +0200

    Update atac.rb

--- Formula/atac.rb
+++ Formula/atac.rb
@@ -1,9 +1,9 @@
 class Atac < Formula
   desc "A simple API client (postman like) in your terminal"
   homepage "https://github.com/Julien-cpsn/ATAC"
-  url "https://github.com/Julien-cpsn/ATAC/releases/download/v0.11.2/atac-x86_64-unknown-linux-musl.tar.gz"
-  version "0.11.2"
-  sha256 "83d6e1a8426206980d5ba6a45b96b25522ab285ed1b88d415c68fe4c34f29cd7"
+  url "https://github.com/Julien-cpsn/ATAC/releases/download/v0.11.3/atac-x86_64-unknown-linux-musl.tar.gz"
+  version "0.11.3"
+  sha256 "f556754af7cc57e2f9a13b753bcc57254b9b1555a280f2ad01020543cde2b396"
   license "MIT"
 
   def install
