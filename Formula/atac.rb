799ac025d412d5b0d3437b9054ab808002b7321d
Julien C <64107022+Julien-cpsn@users.noreply.github.com>
2024-04-15 18:19:12 +0200

    Update atac.rb

--- Formula/atac.rb
+++ Formula/atac.rb
@@ -3,7 +3,7 @@ class Atac < Formula
   homepage "https://github.com/Julien-cpsn/ATAC"
   url "https://github.com/Julien-cpsn/ATAC/releases/download/v0.11.2/atac-universal-apple-darwin.tar.gz"
   version "0.11.2"
-  sha256 "7db198d1144e927816f41836bb492dd183bcb46e18f5584c9c3349c2ad717684"
+  sha256 "7838ac55e0681b9bb25d21b1775a7f0612993212ba9eadbcd3def12345b2e1b1"
   license "MIT"
 
   def install
