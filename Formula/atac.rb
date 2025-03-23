b058886474bc5adb329a9af7e4d530fb4c6dd24f
Julien C. <64107022+Julien-cpsn@users.noreply.github.com>
2025-03-23 22:34:35 +0100

    Update atac.rb

--- Formula/atac.rb
+++ Formula/atac.rb
@@ -1,15 +1,15 @@
 class Atac < Formula
   desc "A simple API client (postman like) in your terminal"
   homepage "https://github.com/Julien-cpsn/ATAC"
-  version "0.18.1"
+  version "0.19.0"
   license "MIT"
 
   if OS.mac?
-    url "https://github.com/Julien-cpsn/ATAC/releases/download/v0.18.1/atac-v0.18.1-universal-apple-darwin.tar.gz"
-    sha256 "c8237984800fe8f1e02080ce48594b418734e16acbe1aa52dc7a1f8c0ff7739d"
+    url "https://github.com/Julien-cpsn/ATAC/releases/download/v0.19.0/atac-v0.19.0-universal-apple-darwin.tar.gz"
+    sha256 "392cbc4d0c59a3d1685bb23cb4fe18e89e79ac55a111a0f32aa47399ed9c674c"
   elsif OS.linux?
-    url "https://github.com/Julien-cpsn/ATAC/releases/download/v0.18.1/atac-v0.18.1-x86_64-unknown-linux-musl.tar.gz"
-    sha256 "7cbab1f2d68cbc9ee04eaddda20f93a17b2c982ed18be819a6f2156fa8aadb1f"
+    url "https://github.com/Julien-cpsn/ATAC/releases/download/v0.19.0/atac-v0.19.0-x86_64-unknown-linux-musl.tar.gz"
+    sha256 "73547bb66686b2a24afd2109cfa02c1303001708d92a3044b30936279b8601f5"
   end
   
   def install
